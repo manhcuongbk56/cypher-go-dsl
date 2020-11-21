393c047a77324986125870a417613fddfbd35322
Manh Cuong <aoevietnam.org@gmail.com>
2020-11-21 18:29:01 +0700

    add node with properties

--- expose_properties.go
+++ expose_properties.go
@@ -1,5 +1,9 @@
 package cypher_go_dsl
 
+import "cypher-go-dsl/expression"
+
 type ExposesProperties interface {
-	WithProperties(keysAndValues ...interface{})
+	WithRawProperties(keysAndValues ...interface{}) (Node, error)
+
+	WithProperties(newProperties expression.MapExpression) Node
 }
--- node.go
+++ node.go
@@ -1,32 +1,45 @@
 package cypher_go_dsl
 
 import (
+	"cypher-go-dsl/expression"
 	v "cypher-go-dsl/visitable"
 )
 
 type Node struct {
-	SymbolicName string
-	Labels []string
-	Properties string
+	symbolicName string
+	labels       []string
+	properties   Properties
 }
 
 func (node Node) Accept(visitor v.Visitor) {
 }
 
-func (node2 Node) RelationshipTo(node Node, types ...string) RelationshipPattern {
+func (node Node) RelationshipTo(nodeDest Node, types ...string) RelationshipPattern {
 	panic("implement me")
 }
 
-func (node2 Node) RelationshipFrom(node Node, types ...string) RelationshipPattern {
+func (node Node) RelationshipFrom(nodeDest Node, types ...string) RelationshipPattern {
 	panic("implement me")
 }
 
-func (node2 Node) RelationshipBetween(node Node, types ...string) RelationshipPattern {
+func (node Node) RelationshipBetween(nodeDest Node, types ...string) RelationshipPattern {
 	panic("implement me")
 }
 
-func (node Node) WithProperties(keysAndValues ...interface{}) {
-	panic("implement me")
+func (node Node) WithRawProperties(keysAndValues ...interface{}) (Node, error){
+	var properties = &expression.MapExpression{}
+	if keysAndValues != nil && len(keysAndValues) != 0 {
+		var err error
+		*properties, err = expression.NewMapExpression(keysAndValues)
+		if err != nil {
+			return Node{}, err
+		}
+	}
+	return node.WithProperties(*properties), nil
+}
+
+func (node Node) WithProperties(newProperties expression.MapExpression) Node {
+	return Node{symbolicName: node.symbolicName, labels: node.labels, properties: Properties{newProperties}}
 }
 
 func (node Node) Property(name string) {
