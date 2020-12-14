0de200587b614ae104a06f14d790bd9cb97b9b2b
Cuong <cuong@com>
2020-12-15 01:10:50 +0700

    add list literal

---
+++ list_literal.go
@@ -0,0 +1,70 @@
+package cypher_go_dsl
+
+import "strings"
+
+type ListLiteral struct {
+	content []Literal
+	key     string
+	err     error
+	notNil  bool
+}
+
+func ListLiteralCreate(content []Literal) ListLiteral {
+	list := ListLiteral{
+		content: content,
+	}
+	list.key = getAddress(&list)
+	return list
+}
+
+func ListLiteralError(err error) ListLiteral {
+	return ListLiteral{
+		err: err,
+	}
+}
+
+func (l ListLiteral) getError() error {
+	return l.err
+}
+
+func (l ListLiteral) accept(visitor *CypherRenderer) {
+	visitor.enter(l)
+	visitor.leave(l)
+}
+
+func (l ListLiteral) enter(renderer *CypherRenderer) {
+	panic("implement me")
+}
+
+func (l ListLiteral) leave(renderer *CypherRenderer) {
+	panic("implement me")
+}
+
+func (l ListLiteral) getKey() string {
+	return l.key
+}
+
+func (l ListLiteral) isNotNil() bool {
+	return l.notNil
+}
+
+func (l ListLiteral) GetExpressionType() ExpressionType {
+	return "ListLiteral"
+}
+
+func (l ListLiteral) GetContent() interface{} {
+	return l.content
+}
+
+func (l ListLiteral) AsString() string {
+	builder := strings.Builder{}
+	builder.WriteString("[")
+	for i, literal := range l.content {
+		if i != 0 {
+			builder.WriteString(", ")
+		}
+		builder.WriteString(literal.AsString())
+	}
+	builder.WriteString("]")
+	return builder.String()
+}
