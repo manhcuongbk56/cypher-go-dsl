6bdafc9ba098d8c1ddb8769631ba89f8faf4dc62
Cuong <cuong@com>
2021-01-02 22:40:52 +0700

    complete list_operator_test.go, add double rendering test

---
+++ test_cypher/cypherit/double_rendering_test.go
@@ -0,0 +1,33 @@
+package cypherit
+
+import (
+	"github.com/manhcuongbk56/cypher-go-dsl"
+	"testing"
+)
+
+func TestFragmentOfStatementShouldBeReusable(t *testing.T) {
+	personNode := cypher.NewNode("Person").NamedByString("p")
+	ageProperty := personNode.Property("age")
+	returning := cypher.MatchElements(personNode).
+		ReturningByString("p")
+	builder1 := returning.OrderBySortItem(ageProperty.Ascending()).Limit(1)
+	builder2 := returning.OrderBySortItem(ageProperty.Descending()).Limit(1)
+	Assert(t, builder1, "MATCH (p:`Person`) RETURN p ORDER BY p.age ASC LIMIT 1")
+	Assert(t, builder2, "MATCH (p:`Person`) RETURN p ORDER BY p.age DESC LIMIT 1")
+}
+
+func TestAliasedFunctionsShouldNotBeRenderedTwiceInProjection(t *testing.T) {
+	o := cypher.NewNode("Order").NamedByString("o")
+	li := cypher.NewNode("LineItem").NamedByString("li")
+	hasLineItems := o.RelationshipTo(li).NamedByString("h")
+	netAmount := cypher.FunctionSum(li.Property("price")).Multiply(li.Property("quantity")).As("netAmount")
+	totalAmount := netAmount.Multiply(cypher.LiteralOf(1).Add(cypher.CypherParameter("taxRate")).Get()).As("totalAmount")
+	returning := cypher.MatchElements(hasLineItems).
+		WhereConditionContainer(o.Property("id").IsEqualTo(cypher.CypherParameter("id"))).
+		With(o.GetRequiredSymbolicName(), netAmount.Get(), totalAmount.Get()).
+		Returning(o.Project(o.Property("x"),
+			netAmount.Get(),
+			totalAmount.Get(),
+			netAmount.Multiply(cypher.CypherParameter("taxRate")).As("taxAmount").Get()))
+	Assert(t, returning, "MATCH (o:`Order`)-[h]->(li:`LineItem`) WHERE o.id = $id WITH o, sum((li.price * li.quantity)) AS netAmount, (netAmount * (1 + $taxRate)) AS totalAmount RETURN o{.x, netAmount: netAmount, totalAmount: totalAmount, taxAmount: (netAmount * $taxRate)}")
+}
--- test_cypher/cypherit/list_operator_test.go
+++ test_cypher/cypherit/list_operator_test.go
@@ -12,3 +12,55 @@ func TestValueAtShouldWork(t *testing.T) {
 
 	Assert(t, builder, "RETURN range(0, 10)[3]")
 }
+
+func TestSubListUntilShouldWork(t *testing.T) {
+	var builder cypher.BuildableStatement
+	//
+	builder = cypher.CypherReturning(cypher.CypherSubListUntil(cypher.FunctionRangeRaw(0, 10), 3))
+
+	Assert(t, builder, "RETURN range(0, 10)[..3]")
+}
+
+func TestSubListFromShouldWork(t *testing.T) {
+	var builder cypher.BuildableStatement
+	//
+	builder = cypher.CypherReturning(cypher.CypherSubListFrom(cypher.FunctionRangeRaw(0, 10), -3))
+
+	Assert(t, builder, "RETURN range(0, 10)[-3..]")
+}
+
+func TestSubListShouldWork(t *testing.T) {
+	var builder cypher.BuildableStatement
+	//
+	builder = cypher.CypherReturning(cypher.CypherSubList(cypher.FunctionRangeRaw(0, 10), 2, 4))
+
+	Assert(t, builder, "RETURN range(0, 10)[2..4]")
+}
+
+func TestShouldWorkWithMapProjections(t *testing.T) {
+	var builder cypher.BuildableStatement
+	person := cypher.NewNode("Person").NamedByString("person")
+	location := cypher.NewNode("Location").NamedByString("personLivesIn")
+	//
+	builder = cypher.
+		MatchElements(person).
+		Returning(person.Project("livesIn", cypher.CypherValueAt(cypher.ListBasedOn(person.RelationshipTo(location, "LIVES_IN")).
+			Returning(location.Project("name")), 0)))
+
+	Assert(t, builder, "MATCH (person:`Person`) RETURN person{livesIn: [(person)-[:`LIVES_IN`]->(personLivesIn:`Location`) | personLivesIn{.name}][0]}")
+}
+
+func TestShouldSupportExpressions(t *testing.T) {
+	var builder cypher.BuildableStatement
+	person := cypher.NewNode("Person").NamedByString("person")
+	location := cypher.NewNode("Location").NamedByString("personLivesIn")
+	//
+	builder = cypher.
+		MatchElements(person).
+		Returning(person.Project("livesIn",
+			cypher.SubList(cypher.ListBasedOn(person.RelationshipTo(location, "LIVES_IN")).
+				Returning(location.Project("name")), cypher.CypherParameter("personLivedInOffset"),
+				cypher.CypherParameter("personLivedInOffset").Add(cypher.CypherParameter("personLivedInFirst")).Get())))
+
+	Assert(t, builder, "MATCH (person:`Person`) RETURN person{livesIn: [(person)-[:`LIVES_IN`]->(personLivesIn:`Location`) | personLivesIn{.name}][$personLivedInOffset..($personLivedInOffset + $personLivedInFirst)]}")
+}
