c536d1b27d9683c53c79326be509bc67af809bef
Cuong <cuong@com>
2021-01-14 21:38:15 +0700

    add test for escape string literal

--- string_literal_test.go
+++ string_literal_test.go
@@ -1,13 +1,29 @@
 package cypher
 
 import (
-	"fmt"
-	"strconv"
 	"testing"
 )
 
-func TestEscapeString(t *testing.T) {
+func TestShouldEscapeContent(t *testing.T) {
 	origin := "A\\B\\\\Ca'bc123\\"
-	escaped := strconv.Quote(origin)
-	fmt.Println(escaped)
+	literal := StringLiteral{content: origin}
+	escaped := literal.AsString()
+	if escaped != "'A\\\\B\\\\\\\\Ca\\'bc123\\\\'" {
+		t.Errorf("escaped is not match:\n %s", escaped)
+	}
+}
+
+
+func TestShouldEscapeEmptyString(t *testing.T) {
+	inputs := make([][]string, 0)
+	inputs = append(inputs, []string{"", ""})
+	inputs = append(inputs, []string{" \t ", " \t "})
+	inputs = append(inputs, []string{"Nothing to escape", "Nothing to escape"})
+	inputs = append(inputs, []string{"' \" '", "\\' \\\" \\'"})
+	for _, input := range inputs {
+		escaped := escapeStringLiteral(input[0])
+		if escaped != input[1] {
+			t.Errorf("escaped is not match:\n %s, expect is:\n %s", escaped, input[1])
+		}
+	}
 }
